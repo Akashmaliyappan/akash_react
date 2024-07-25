f1c2a4556b49d474c6bfd109573f40bca7280b4a
Akashmaliyappan <163811988+Akashmaliyappan@users.noreply.github.com>
2024-07-25 20:24:04 +0530

    Update Bike.jsx

--- src/components/Bike.jsx
+++ src/components/Bike.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-
+import r15 from './src/assets/r15.jpg'
 
 
 
 const Bikes = [
     {
         id: 1,
-        bikeimage: './src/assets/r15.jpg',
+        bikeimage: {r15},
         name: 'R15',
         color: 'Blue',
         year: '2023',
